4264addbb4fc9b80cc499befe9d3448dbb71f323
Alexis Hernández Castro <alex.hdz.c@ciencias.unam.mx>
2019-03-17 15:08:58 -0600

    Prueba Netbeams

--- target/generated-sources/annotations/com/ingenieria/biblioteca/modelo/Profesor_.java
+++ target/generated-sources/annotations/com/ingenieria/biblioteca/modelo/Profesor_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-16T19:05:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-17T15:04:44")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
